b38920e9b377023f97beb70ba8684394e4278089
Jack0423 <jack940423@gmail.com>
2022-03-15 10:41:04 +0800

    Initial Commit

---
+++ school project/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  school project
+//
+//  Created by Jack on 2022/3/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ school project/school_projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  school_projectApp.swift
+//  school project
+//
+//  Created by Jack on 2022/3/15.
+//
+
+import SwiftUI
+
+@main
+struct school_projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
