3ef7751d2885d75d66cc2d5ad75aecdfea73f203
Raylee0306-VMmacbookair <ray0306.lee@gmail.com>
2022-06-18 16:46:58 +0800

    0

--- school project/ContentView.swift
+++ school project/ContentView.swift
@@ -13,6 +13,7 @@ import SwiftUI
     
 struct ContentView: View {
     @State private var navigateTo: AnyView?
+    //@State private var navigateTo :String
     @State private var isNavigaionActive = false
     let imgheight = UIScreen.main.bounds.height -  (UIApplication.shared.statusBarFrame.size.height + 140)
     
@@ -25,15 +26,16 @@ struct ContentView: View {
                             .font(.largeTitle.bold())
                         Spacer()
                         
+                        
                         Menu {
                            Button {
-                               navigateTo = AnyView(information())
+                               navigateTo = AnyView(birdmenu())
                                isNavigaionActive = true
                            } label: {
+                               
                                Image(systemName: "info.circle")
-                                   .padding(.horizontal)
-                                   .font(.largeTitle)
                                    .foregroundColor(.black)
+                               Spacer()
                                Text("關於APP")
                            }
                            Button {
@@ -41,141 +43,126 @@ struct ContentView: View {
                                isNavigaionActive = true
                            } label: {
                                Image(systemName: "square.and.pencil.circle")
-                                   .padding(.horizontal)
-                                   .font(.largeTitle)
                                    .foregroundColor(.black)
+                               Spacer()
                                Text("意見回饋")
                            }
                         } label: {
                            Image(systemName: "list.bullet")
-                               .padding(.horizontal)
+                               .padding()
                                .font(.largeTitle)
                                .foregroundColor(.blue)
-                        }   /*.background(/Volumes/SFSymbols/SF Symbols.pkg
-                                NavigationLink(destination: Text(self.navigateTo), isActive: $isActive) {
+                        }
+                        /*
+                        Menu {
+                           Button {
+                               self.navigateTo = "birdmenu()"
+                               isNavigaionActive = true
+                           } label: {
+                               
+                               Image(systemName: "info.circle")
+                                   .foregroundColor(.black)
+                               Spacer()
+                               Text("關於APP")
+                           }
+                           Button {
+                               self.navigateTo = "feedback()"
+                               isNavigaionActive = true
+                           } label: {
+                               Image(systemName: "square.and.pencil.circle")
+                                   .foregroundColor(.black)
+                               Spacer()
+                               Text("意見回饋")
+                           }
+                        } label: {
+                           Image(systemName: "list.bullet")
+                               .padding()
+                               .font(.largeTitle)
+                               .foregroundColor(.blue)
+                        }   .background(
+                                NavigationLink(destination: self.navigateTo, isActive: $isNavigaionActive) {
                                     EmptyView()
                                 }
-                            )*/
-                        
-                        /*
-                        NavigationLink {
-                            Text("23")
-                            } label: {
-                                
-                            Image(systemName: "gearshape")
-                                .padding()
-                                .font(.largeTitle)
-                                .foregroundColor(.blue)
-                        }
+                            )
                         */
                         
                     }.offset(y:10)
                 
-                //ScrollView {
-                    ScrollView(.horizontal, showsIndicators: false) {
-                        HStack(spacing:20){
-                            NavigationLink{
-                                birdmenu()
-                            }label: {
-                                ZStack{
-                                    Image("bird")
-                                        .resizable()
-                                        .scaledToFill()
-                                        .frame(width: 350, height: imgheight )
-                                        .clipped()
-                                        .cornerRadius(30)
-                                    Text("水鳥")
-                                        .fontWeight(.bold)
-                                        .font(.system(size: 35, design: .rounded))
-                                        .foregroundColor(.black)
-                                        .padding(EdgeInsets(top: 0, leading: 125, bottom: 0, trailing: 125))
-                                        .cornerRadius(10)
-                                        .padding(EdgeInsets(top: imgheight-40, leading: 15, bottom: 0, trailing: 15))
-                                        .background(LinearGradient(gradient: Gradient(colors: [Color.clear, Color.white]), startPoint: .top, endPoint: .bottom))
-                                        .cornerRadius(30)
-                                        .padding()
-                                }
-                            }
-                            
-                            NavigationLink{
-                                plant()
-                            }label: {
-                                ZStack{
-                                    Image("plant")
-                                        .resizable()
-                                        .scaledToFill()
-                                        .frame(width: 350, height: imgheight)
-                                        .clipped()
-                                        .cornerRadius(30)
-                                    Text("植物")
-                                        .fontWeight(.bold)
-                                        .font(.system(size: 35, design: .rounded))
-                                        .foregroundColor(.black)
-                                        .padding(EdgeInsets(top: 0, leading: 125, bottom: 0, trailing: 125))
-                                        .cornerRadius(10)
-                                        .padding(EdgeInsets(top: imgheight-40, leading: 15, bottom: 0, trailing: 15))
-                                        .background(LinearGradient(gradient: Gradient(colors: [Color.clear, Color.white]), startPoint: .top, endPoint: .bottom))
-                                        .cornerRadius(30)
-                                        .padding()
-                                }
-                            }
-                            
-                            NavigationLink{
-                                bruh()
-                            }label: {
-                                ZStack{
-                                    Image("place")
-                                        .resizable()
-                                        .scaledToFill()
-                                        .frame(width: 350, height: imgheight)
-                                        .clipped()
-                                        .cornerRadius(30)
-                                    Text("地圖")
-                                        .fontWeight(.bold)
-                                        .font(.system(size: 35, design: .rounded))
-                                        .foregroundColor(.black)
-                                        .padding(EdgeInsets(top: 0, leading: 125, bottom: 0, trailing: 125))
-                                        .cornerRadius(10)
-                                        .padding(EdgeInsets(top: imgheight-40, leading: 15, bottom: 0, trailing: 15))
-                                        .background(LinearGradient(gradient: Gradient(colors: [Color.clear, Color.white]), startPoint: .top, endPoint: .bottom))
-                                        .cornerRadius(30)
-                                        .padding()
-                                }
-                            }
-
-                        }//HStack
-                    }.offset(y: -15)//scrollview
-                //}//Scrollview
-            }.navigationBarHidden(true)
-            //.navigationBarTitleDisplayMode(.inline)
                 
-            //VStack
-                /*
-                .navigationTitle("選取主題")
-                .navigationBarItems(
-                    trailing: NavigationLink (
-                        destination: settings()
-                    ) {
-                        Image(systemName: "gearshape")
-                            .font(.largeTitle)
-                            .foregroundColor(.blue)
-                            .offset(y: 50)
-                    }
-                )*/
-                /*.toolbar(content: {
-                    ToolbarItem {
-                        NavigationLink {
-                            settings()
-                        } label: {
-                            Image(systemName: "gearshape")
-                                .font(.largeTitle)
-                                .foregroundColor(.blue)
-                                .offset(y: 50)
+                ScrollView(.horizontal, showsIndicators: false) {
+                    HStack(spacing:20){
+                        NavigationLink{
+                            birdmenu()
+                        }label: {
+                            ZStack{
+                                Image("bird")
+                                    .resizable()
+                                    .scaledToFill()
+                                    .frame(width: 350, height: imgheight )
+                                    .clipped()
+                                    .cornerRadius(30)
+                                Text("水鳥")
+                                    .fontWeight(.bold)
+                                    .font(.system(size: 35, design: .rounded))
+                                    .foregroundColor(.black)
+                                    .padding(EdgeInsets(top: 0, leading: 125, bottom: 0, trailing: 125))
+                                    .cornerRadius(10)
+                                    .padding(EdgeInsets(top: imgheight-40, leading: 15, bottom: 0, trailing: 15))
+                                    .background(LinearGradient(gradient: Gradient(colors: [Color.clear, Color.white]), startPoint: .top, endPoint: .bottom))
+                                    .cornerRadius(30)
+                                    .padding()
+                            }
                         }
-                    }
-                })*/
+                        
+                        NavigationLink{
+                            plant()
+                        }label: {
+                            ZStack{
+                                Image("plant")
+                                    .resizable()
+                                    .scaledToFill()
+                                    .frame(width: 350, height: imgheight)
+                                    .clipped()
+                                    .cornerRadius(30)
+                                Text("植物")
+                                    .fontWeight(.bold)
+                                    .font(.system(size: 35, design: .rounded))
+                                    .foregroundColor(.black)
+                                    .padding(EdgeInsets(top: 0, leading: 125, bottom: 0, trailing: 125))
+                                    .cornerRadius(10)
+                                    .padding(EdgeInsets(top: imgheight-40, leading: 15, bottom: 0, trailing: 15))
+                                    .background(LinearGradient(gradient: Gradient(colors: [Color.clear, Color.white]), startPoint: .top, endPoint: .bottom))
+                                    .cornerRadius(30)
+                                    .padding()
+                            }
+                        }
+                        
+                        NavigationLink{
+                            bruh()
+                        }label: {
+                            ZStack{
+                                Image("place")
+                                    .resizable()
+                                    .scaledToFill()
+                                    .frame(width: 350, height: imgheight)
+                                    .clipped()
+                                    .cornerRadius(30)
+                                Text("地圖")
+                                    .fontWeight(.bold)
+                                    .font(.system(size: 35, design: .rounded))
+                                    .foregroundColor(.black)
+                                    .padding(EdgeInsets(top: 0, leading: 125, bottom: 0, trailing: 125))
+                                    .cornerRadius(10)
+                                    .padding(EdgeInsets(top: imgheight-40, leading: 15, bottom: 0, trailing: 15))
+                                    .background(LinearGradient(gradient: Gradient(colors: [Color.clear, Color.white]), startPoint: .top, endPoint: .bottom))
+                                    .cornerRadius(30)
+                                    .padding()
+                            }
+                        }
+
+                    }//HStack
+                }.offset(y: -15)//scrollview
+            }.navigationBarHidden(true)//VSyack
         }//navigationview
     }//var body: some view
 }
-
-
