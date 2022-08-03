7c7b8c54a2afaa31c223d4a7e0ddd20789056376
Raylee0306-VMmacbookair <ray0306.lee@gmail.com>
2022-08-03 22:51:11 +0800

    add detail website of birdimages in information

--- school project/information.swift
+++ school project/information.swift
@@ -10,7 +10,13 @@ import SwiftUI
 
 struct information: View {
     var body: some View{
-        Text("製作人：王凱立、李泫翰\n\n如有任何版權問題\n請使用意見回饋或下方的email告訴我們\njack940423@gmail.com\nray0306.lee@gmail.com")
+        Text("製作人：王凱立、李泫翰\n")
+            .fontWeight(.medium)
+            .font(.body)
+        Text("鳥類圖片詳細來源：https://hackmd.io/@ios-app/BJ7Bqdp-q")
+            .fontWeight(.medium)
+            .font(.body)
+        Text("如有任何版權問題\n請使用意見回饋或下方的email告訴我們\n王凱立：jack940423@gmail.com\n李泫翰：ray0306.lee@gmail.com")
             .fontWeight(.medium)
             .font(.body)
     }
