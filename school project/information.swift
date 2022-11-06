3b03ffccb1bad3045f3ec79a1d4688b9ec7b46c8
Raylee0306 <ray0306.lee2gmail.com>
2022-11-06 22:25:39 +0800

    修改「關於APP」

--- school project/information.swift
+++ school project/information.swift
@@ -9,15 +9,21 @@ import Foundation
 import SwiftUI
 
 struct information: View {
+    let wordsize = UIScreen.main.bounds.width * 0.045
     var body: some View{
-        Text("製作人：王凱立、李泫翰\n")
-            .fontWeight(.medium)
-            .font(.body)
-        Text("鳥類圖片詳細來源：\nhttps://hackmd.io/@ios-app/BJ7Bqdp-q")
-            .fontWeight(.medium)
-            .font(.body)
-        Text("如有任何版權問題\n請使用意見回饋或下方的email告訴我們\n王凱立：jack940423@gmail.com\n李泫翰：ray0306.lee@gmail.com")
-            .fontWeight(.medium)
-            .font(.body)
+        VStack(alignment: .leading){
+            Text("製作人：王凱立、李泫翰")
+            Text("如有任何版權問題")
+            Text("請使用意見回饋或下方的email告訴我們吧！")
+            Text("官方：ckshapp@cksh.hc.edu.tw")
+            Text("王凱立：jack940423@gmail.com")
+            Text("李泫翰：ray0306.lee@gmail.com")
+            Text("\n鳥類圖片詳細來源：")
+            Text("https://hackmd.io/@ios-app/BJ7Bqdp-q")
+            Text("建功高中校網連結：")
+            Text("https://www.cksh.hc.edu.tw/")
+        }.font(.system(size: wordsize, design: .rounded))
+            .foregroundColor(.black)
+            .padding()
     }
 }
